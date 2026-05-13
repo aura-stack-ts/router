2090a510f3fe618554c0731d08984bb4e4d93dd6
Hernan Alvarado <hernanvid123@gmail.com>
2026-05-13 11:37:30 -0500

    docs: refresh documentation and changelog for latest updates

--- docs/src/mdx-components.tsx
+++ docs/src/mdx-components.tsx
@@ -2,12 +2,14 @@ import defaultMdxComponents from "fumadocs-ui/mdx"
 import type { MDXComponents } from "mdx/types"
 import * as Twoslash from "fumadocs-twoslash/ui"
 import * as StepComponents from "fumadocs-ui/components/steps"
+import * as TabsComponents from "fumadocs-ui/components/tabs"
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
     return {
         ...defaultMdxComponents,
         ...Twoslash,
         ...StepComponents,
+        ...TabsComponents,
         ...components,
     }
 }
